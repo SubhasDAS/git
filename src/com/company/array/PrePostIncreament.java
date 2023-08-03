0b6adbf73acbe20e23a684cebed5a64cab827aad
SubhasDAS <ubhasdas2012@gmail.com>
2023-08-03 20:08:00 +0530

    Package name changed

---
+++ src/com/company/array/PrePostIncreament.java
@@ -0,0 +1,21 @@
+package com.company.array;
+
+public class PrePostIncreament {
+
+	
+	public static void main(String[] args)
+	{
+		
+		int i=25;
+		int j = i++;
+		System.out.println("i= "+i);
+		System.out.println("i= "+j);
+		
+		int m=25;
+		int n = ++m;
+		System.out.println("m= "+m);
+		System.out.println("m= "+n);
+		
+
+	}
+}
